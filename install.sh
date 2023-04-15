da3cdb51e0b938f77edf763aac9ee9ad423be217
h00dy <101464998+hoodietramp@users.noreply.github.com>
2023-04-16 00:04:25 +0530

    that' it

--- install.sh
+++ install.sh
@@ -1,64 +1,65 @@
 #!/bin/bash
 
 read -p "enter your sudo password to continue: " pass
-requirements="\033[32m[+] Downloading requirements...\033[0m"
+requirements="[+] Downloading requirements..."
 
 for ((i=0; i<${#requirements}; i++)); do
     echo -ne "${requirements:$i:1}"
-    sleep 0.005
+    sleep 0.07
 done
 
 echo $pass | sudo -S apt install xclip
 
-clone_text="\033[32m[+] Cloning the repo...\033[0m"
+clone_text="[+] Cloning the repo..."
 for ((i=0; i<${#clone_text}; i++)); do
     echo -ne "${clone_text:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
 git clone https://github.com/hoodietramp/tmux_config.git
 cd tmux_config
-setup="\033[32m[+] Setting up tmux config...\033[0m"
+setup="[+] Setting up tmux config..."
 for ((i=0; i<${#setup}; i++)); do
     echo -ne "${setup:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
 cp updatedtmux.conf ~/.tmux.conf
 mkdir -p ~/Scripts
 cp updated_vpnIP_VM.sh ~/Scripts/vpnIP_VM.sh
 
-tmux_plugin="\033[32madding tmux plugin...\033[0m"
+tmux_plugin="adding tmux plugin..."
 for ((i=0; i<${#tmux_plugin}; i++)); do
     echo -ne "${tmux_plugin:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Press Prefix+I to install the plugins"
 
-icons_install="\033[32m[+] Installing icons in terminal...\033[0m"
+icons_install="[+] Installing icons in terminal..."
 for ((i=0; i<${#icons_install}; i++)); do
     echo -ne "${icons_install:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
 cd ~/ ; git clone https://github.com/sebastiencs/icons-in-terminal.git
 cd icons-in-terminal
 ./install-autodetect.sh
 
-setup_done="\033[32m[-] D0ne!\033[0m"
+setup_done="[-] D0ne!"
 for ((i=0; i<${#setup_done}; i++)); do
     echo -ne "${setup_done:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
-exit_txt="\033[32mExiting Terminal Now...\033[0m"
+exit_txt="Exiting Terminal Now..."
 for ((i=0; i<${#exit_txt}; i++)); do
     echo -ne "${exit_txt:$i:1}"
-    sleep 0.05
+    sleep 0.07
 done
 
 echo "open new terminal for the change."
 sleep 1
+
 exit
