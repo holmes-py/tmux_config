a45f0c74d35c3d08a11002e57ca1d4a2c6137e2f
h00dy <101464998+hoodietramp@users.noreply.github.com>
2023-04-15 23:41:23 +0530

    added into install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ for ((i=0; i<${#requirements}; i++)); do
     # Print the current digit
     echo -n "${requirements:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 echo $pass | sudo -S apt install xclip
 clone_text="[+] Cloning the repo..."
@@ -14,7 +14,7 @@ for ((i=0; i<${#clone_text}; i++)); do
     # Print the current digit
     echo -n "${clone_text:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 git clone https://github.com/hoodietramp/tmux_config.git
 cd tmux_config
@@ -23,7 +23,7 @@ for ((i=0; i<${#setup}; i++)); do
     # Print the current digit
     echo -n "${setup:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 cp updatedtmux.conf ~/.tmux.conf
 mkdir -p ~/Scripts
@@ -33,7 +33,7 @@ for ((i=0; i<${#tmux_plugin}; i++)); do
     # Print the current digit
     echo -n "${tmux_plugin:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Press Prefix+I to install the plugins"
@@ -42,7 +42,7 @@ for ((i=0; i<${#icons_install}; i++)); do
     # Print the current digit
     echo -n "${icons_install:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 cd ~/ ; git clone https://github.com/sebastiencs/icons-in-terminal.git
 cd icons-in-terminal
@@ -52,14 +52,14 @@ for ((i=0; i<${#setup_done}; i++)); do
     # Print the current digit
     echo -n "${setup_done:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.1
+    sleep 0.01
 done
 exit_txt="Exiting Terminal Now..."
 for ((i=0; i<${#exit_txt}; i++)); do
     # Print the current digit
     echo -n "${exit_txt:$i:1}"
     # Wait for 0.5 seconds
-    sleep 0.2
+    sleep 0.02
 done
 echo "open new terminal for the change."
 sleep 1
