dbd77b23dbb94b65e53c80e348e1c7096504395a
h00dy <101464998+hoodietramp@users.noreply.github.com>
2023-04-16 00:19:00 +0530

    fixed errors

--- install.sh
+++ install.sh
@@ -4,25 +4,28 @@ read -p "enter your sudo password to continue: " pass
 requirements="[+] Downloading requirements..."
 
 for ((i=0; i<${#requirements}; i++)); do
-    echo -ne "${requirements:$i:1}"
+    echo -n "${requirements:$i:1}"
     sleep 0.07
 done
+echo
 
 echo $pass | sudo -S apt install xclip
 
 clone_text="[+] Cloning the repo..."
 for ((i=0; i<${#clone_text}; i++)); do
-    echo -ne "${clone_text:$i:1}"
+    echo -n "${clone_text:$i:1}"
     sleep 0.07
 done
+echo
 
 git clone https://github.com/hoodietramp/tmux_config.git
 cd tmux_config
 setup="[+] Setting up tmux config..."
 for ((i=0; i<${#setup}; i++)); do
-    echo -ne "${setup:$i:1}"
+    echo -n "${setup:$i:1}"
     sleep 0.07
 done
+echo
 
 cp updatedtmux.conf ~/.tmux.conf
 mkdir -p ~/Scripts
@@ -33,6 +36,7 @@ for ((i=0; i<${#tmux_plugin}; i++)); do
     echo -ne "${tmux_plugin:$i:1}"
     sleep 0.07
 done
+echo
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Press Prefix+I to install the plugins"
@@ -42,6 +46,7 @@ for ((i=0; i<${#icons_install}; i++)); do
     echo -ne "${icons_install:$i:1}"
     sleep 0.07
 done
+echo
 
 cd ~/ ; git clone https://github.com/sebastiencs/icons-in-terminal.git
 cd icons-in-terminal
@@ -52,14 +57,15 @@ for ((i=0; i<${#setup_done}; i++)); do
     echo -ne "${setup_done:$i:1}"
     sleep 0.07
 done
+echo
 
-exit_txt="Exiting Terminal Now..."
+exit_txt="Exiting Script Now..."
 for ((i=0; i<${#exit_txt}; i++)); do
     echo -ne "${exit_txt:$i:1}"
     sleep 0.07
 done
+echo
 
 echo "open new terminal for the change."
-sleep 1
 
 exit
