ef3f84a933c266a437e0716ea0d906037214c9e0
h00dy <101464998+hoodietramp@users.noreply.github.com>
2023-04-15 23:57:46 +0530

    that should do it

--- install.sh
+++ install.sh
@@ -1,53 +1,64 @@
 #!/bin/bash
 
 read -p "enter your sudo password to continue: " pass
-requirements="[+] Downloading requirements..."
+requirements="\033[32m[+] Downloading requirements...\033[0m"
 
 for ((i=0; i<${#requirements}; i++)); do
-    echo -ne "\033[32m${requirements:$i:1}\033[0m"
-    sleep 0.001
+    echo -ne "${requirements:$i:1}"
+    sleep 0.005
 done
+
 echo $pass | sudo -S apt install xclip
-clone_text="[+] Cloning the repo..."
+
+clone_text="\033[32m[+] Cloning the repo...\033[0m"
 for ((i=0; i<${#clone_text}; i++)); do
-    echo -ne "\033[32m${clone_text:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${clone_text:$i:1}"
+    sleep 0.05
 done
+
 git clone https://github.com/hoodietramp/tmux_config.git
 cd tmux_config
-setup="[+] Setting up tmux config..."
+setup="\033[32m[+] Setting up tmux config...\033[0m"
 for ((i=0; i<${#setup}; i++)); do
-    echo -ne "\033[32m${setup:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${setup:$i:1}"
+    sleep 0.05
 done
+
 cp updatedtmux.conf ~/.tmux.conf
 mkdir -p ~/Scripts
 cp updated_vpnIP_VM.sh ~/Scripts/vpnIP_VM.sh
-tmux_plugin="adding tmux plugin..."
+
+tmux_plugin="\033[32madding tmux plugin...\033[0m"
 for ((i=0; i<${#tmux_plugin}; i++)); do
-    echo -ne "\033[32m${tmux_plugin:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${tmux_plugin:$i:1}"
+    sleep 0.05
 done
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Press Prefix+I to install the plugins"
-icons_install="[+] Installing icons in terminal..."
+
+icons_install="\033[32m[+] Installing icons in terminal...\033[0m"
 for ((i=0; i<${#icons_install}; i++)); do
-    echo -ne "\033[32m${icons_install:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${icons_install:$i:1}"
+    sleep 0.05
 done
+
 cd ~/ ; git clone https://github.com/sebastiencs/icons-in-terminal.git
 cd icons-in-terminal
 ./install-autodetect.sh
-setup_done="[-] D0ne!"
+
+setup_done="\033[32m[-] D0ne!\033[0m"
 for ((i=0; i<${#setup_done}; i++)); do
-    echo -ne "\033[32m${setup_done:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${setup_done:$i:1}"
+    sleep 0.05
 done
-exit_txt="Exiting Terminal Now..."
+
+exit_txt="\033[32mExiting Terminal Now...\033[0m"
 for ((i=0; i<${#exit_txt}; i++)); do
-    echo -ne "\033[32m${exit_txt:$i:1}\033[0m]"
-    sleep 0.001
+    echo -ne "${exit_txt:$i:1}"
+    sleep 0.05
 done
+
 echo "open new terminal for the change."
 sleep 1
 exit
